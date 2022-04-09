f53dce12392d6d5f9206fa5afd4c3b5233fd7985
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2022-04-08 20:21:15 -0500

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,6 @@
 // Store our API endpoint as queryUrl.
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-
 // Perform a GET request to the query URL.
 d3.json(queryUrl).then(function (data) {
   createMap(data.features);
@@ -107,9 +106,6 @@ function createMap(earthquakes) {
       }
     }).addTo(tectonicLayer);
   }); 
-
-
-
 
   // Create the base layers.
 
